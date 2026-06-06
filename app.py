06d333278e8da5ea4948178da54781a7a3bb740e
yahirtaz12 <yahir-taz@hotmail.com>
2026-06-06 14:12:48 -0600

    Configurar app para Render

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import random
 import string
 import sqlite3
 from flask import Flask, render_template, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -140,4 +141,5 @@ def validar():
          })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
